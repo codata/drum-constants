97442e819c0dcf185bbb9c5ca23e35ee3fcaca15
kulnor <pascal.heus@postman.com>
2024-08-05 14:53:02 -0600

    Added code to consolidate and assign NIST identifiers to quantities

--- nist/README.md
+++ nist/README.md
@@ -9,7 +9,7 @@ Beyond consulting the NIST website, the values for the constants are available f
 
 ### ASCII data
 
-The latest values for the constants can be found [here]( https://pml.nist.gov/cuu/Constants/Table/allascii.txt), with previous versions available in archived sections: [2018](https://pml.nist.gov/cuu/Constants/archive2018.html),[2014](https://pml.nist.gov/cuu/Constants/archive2014.html), [2010](https://pml.nist.gov/cuu/Constants/archive2010.html), [2006](https://pml.nist.gov/cuu/Constants/archive2006.html), [2002](https://pml.nist.gov/cuu/Constants/archive2002.html), and [1998](https://pml.nist.gov/cuu/Constants/archive1998.html).Note that a formatting change occured in 2010 (separating value and uncertainty). Only PDFs are avaible for [1986](https://pml.nist.gov/cuu/Constants/archive1986.html)),[1973](https://pml.nist.gov/cuu/Constants/archive1973.html)), and [1969](https://pml.nist.gov/cuu/Constants/archive1969.html)) 
+The latest values for the constants can be found [here](https://pml.nist.gov/cuu/Constants/Table/allascii.txt), with previous versions available in archived sections: [2018](https://pml.nist.gov/cuu/Constants/archive2018.html),[2014](https://pml.nist.gov/cuu/Constants/archive2014.html), [2010](https://pml.nist.gov/cuu/Constants/archive2010.html), [2006](https://pml.nist.gov/cuu/Constants/archive2006.html), [2002](https://pml.nist.gov/cuu/Constants/archive2002.html), and [1998](https://pml.nist.gov/cuu/Constants/archive1998.html).Note that a formatting change occured in 2010 (separating value and uncertainty). Only PDFs are avaible for [1986](https://pml.nist.gov/cuu/Constants/archive1986.html)),[1973](https://pml.nist.gov/cuu/Constants/archive1973.html)), and [1969](https://pml.nist.gov/cuu/Constants/archive1969.html)) 
 
 As previously mentionned, the primary ASCII file is formatted for a human reader. Notably:
 - It starts with a few lines documenting its content
@@ -19,8 +19,25 @@ As previously mentionned, the primary ASCII file is formatted for a human reader
 - When the constant is precisely determined, its uncertainty numeric value is the string `(exact)`
 - The records are in fixed ASCII columns (after the multiline header)
 
-Note also that, while NIST has internal identifiers associated with each constant, these are no present in the main ASCII file. We must therefore map the quantities' names, whose value may have changed over time, to persistent global identifiers.
+Note also that, while NIST has internal identifiers associated with each constant, these are no present in the main ASCII file. We must therefore map the quantities' names, whose value may have changed over time, to persistent global identifiers (see below).
 
 All these aspects make it impossible for machines to use the NIST publised infornation as is. It must therefore be transformed for further use by applications, databases, APIs, LLMS, and other digital processing purposes. Utilities to do so are provided in this section.
 
+The `nist_to_json.py` script takes care of this to generate a new JSON file for the ASCII files, performing various cleansing, conversions, and derivation. These files can be found in the version sub-directories.
 
+### NIST identifiers
+
+NIST uses its own unique indentifiers for the fundemantal constants (a few letters and numbers), which you can be seen in the web pages URL and other places (e.g. `alpha particle mass` is `mal`). 
+These identifiers are unfortunately not formally maintained and published, nor present in the published allascii files.
+They also do not have documented crosswalks to other widely used ontologies such as QUDT .
+
+The web page showing the [list of all constants](https://physics.nist.gov/cgi-bin/cuu/Category?vie) can be used to scrape the identifiers and generic names. This however does not work 
+across all quantities or versions as names in the ASCII files may be abbreviated (e.g. ) or have changed over time, and some quantities have been addede/dropped.
+
+Interestingly, the quantity names and their identifiers are present in the correlation coefficient ascii files (e.g. [2018](https://pml.nist.gov/cuu/Constants/ArchiveASCII/corrcoef2018)), and therefore can be extracted with a little parsing. For some reason this ASCII file does not seem to be available for the latest/current version (only archived years).
+
+We have implemented helper methods to produce a consolidated [nist_ids.json](nist_ids.json) file holding the identifier and the quantiy names (in occasional cases more than one).
+This is used during the allascii file processing to assign identifiers to the quantities.
+These may need adjustments when future versions are released.
+
+Note that the indentifers are case sensitive as two entries share a similar identifier (`Ae` and `ae`). This is normal behaviour anyway.
--- nist/nist_to_json.py
+++ nist/nist_to_json.py
@@ -11,11 +11,15 @@ For a copy, see <https://opensource.org/licenses/MIT>.
 """
 
 from dataclasses import dataclass, asdict, field
+from functools import cache
+import logging
 import re
 from typing import List, Optional
 import json
 import argparse
 import os
+
+ALL_VERSIONS = [1998,2002,2006,2010,2014,2018,2022]
 
 @dataclass
 class PhysicalConstant:
@@ -24,6 +28,7 @@ class PhysicalConstant:
     nist_value: str
     nist_uncertainty: Optional[str]
     unit: Optional[str]
+    nist_id: Optional[str] = field(init=False, default=None)
     exponent: Optional[str] = field(init=False, default=None)
     is_exact: bool = field(init=False, default=False)
     is_truncated: bool = field(init=False, default=False)
@@ -93,6 +98,7 @@ class PhysicalConstant:
         if not self.is_exact:
             self.str_uncertainty = self.nist_uncertainty.replace(' ', '')
             self.numeric_uncertainty = float(self.str_uncertainty)
+        
 
 class PhysicalConstantEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -101,6 +107,126 @@ class PhysicalConstantEncoder(json.JSONEncoder):
             return result
         return super().default(obj)
 
+@cache
+def get_corrcoeff(year):
+    """
+    Returns the correlation coefficient ASCII file for the given year if it exists.
+    """
+    corrceoff_filepath = os.path.join(script_dir, str(year), f'corrcoef{year}.txt')
+    if os.path.isfile(corrceoff_filepath):
+        with open(corrceoff_filepath, 'r') as f:
+            lines = f.readlines()
+            return lines
+    else:
+        return None
+
+@cache
+def get_corrcoeff_id_name(year):
+    """
+    Extract a dictionary of id/name pairs from the correlation coefficient ASCII file.
+    """
+    data = {}
+    if get_corrcoeff(year):
+        for line in get_corrcoeff(year):
+            if "---" in line:
+                pattern = r'(\S+)\s*---\s*(.+)'
+                m = re.search(pattern, line)
+                if m:
+                    id = m.group(1)
+                    name = m.group(2).strip()
+                    data[id] = name
+    return data
+
+
+@cache
+def get_corrcoeff_name_id(year):
+    """
+    Extract a dictionary of name/id pairs from the correlation coefficient ASCII file.
+    """
+    data = get_corrcoeff_id_name(year)
+    if data:
+        data = {value: key for key, value in data.items()}
+        return data
+
+
+@cache
+def get_nist_ids():
+    """
+    Loads and returns the NIST identifiers from the JSON file.
+    """
+    filepath = os.path.join(script_dir, 'nist_ids.json')
+    with open(filepath, 'r') as f:
+        return json.load(f)
+
+@cache
+def get_nist_names():
+    """
+    Loads and returns the NIST names and their identifier.
+    """
+    data = {}
+    nist_ids = get_nist_ids()
+    for id,names in nist_ids.items():
+        for name in names:
+            data[name] = id
+    return data    
+
+
+def ids_to_json():
+    """
+    Produces a json file with the know NIST identifiers and names.
+   
+    This collects from the correlation coefficient ASCII files across all available versions.
+    Some names are not in the ASCII files and manually aded to the dictionnary.
+    The first entry in the array is considered the preferred one.
+    
+    """
+    data = {}
+    # Know values / entries not in corrcoeff files
+    data['Ae'] = ["atomic unit of charge"]
+    data['charge90'] = ["conventional value of coulomb-90"]
+    data["e"] = ["elementary charge"]
+    data['ral'] = ["alpha particle rms charge radius"]
+    data['mtauc2mev'] = ["tau mass energy equivalent in MeV"] # also known as tau energy equivalent (but this seems less acccurate)
+    # load identifier data from corrcoeff files
+    for year in reversed(ALL_VERSIONS): # reverse order to get most recent entry first (preferred)
+        year_data = get_corrcoeff_id_name(year)
+        if year_data:
+            for id, name in year_data.items():
+                if id in data:
+                    # add additional name if needed
+                    if name not in data[id]:
+                        data[id].append(name) 
+                else:
+                    # create new entry
+                    data[id] = [name] 
+    # alternate names
+    data['d220sil'].append("{220} lattice spacing of silicon") # known as 'lattice spacing of Si (220)'
+    # cleanup
+    del data['Constants'] # rogue entry from one of the corrcoeff files
+    # sort by key
+    data = dict(sorted(data.items()))  
+    # write to json file
+    with open('nist_ids.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+    
+@cache
+def lookup_id(name):
+    """
+    Lookup the NIST identifier for the given name.
+    
+    Note that some abbreviations are expanded before lookup.
+
+    """
+    # expand known abbreviations
+    if 'mag.' in name:
+        name = name.replace('mag.', 'magnetic')
+    if 'mom.' in name:
+        name = name.replace('mom.', 'moment')
+    # lookup
+    id = get_nist_names().get(name)
+    return id
+    
 def read_allascii_file(filename: str, year: int) -> List[PhysicalConstant]:
     constants = []
     with open(filename, 'r') as file:
@@ -132,36 +258,54 @@ def read_allascii_file(filename: str, year: int) -> List[PhysicalConstant]:
             constants.append(constant)
     
     return constants
-
+    
 def allascii_to_json(constants: List[PhysicalConstant], filename: str):
     with open(filename, 'w') as f:
         json.dump(constants, f, cls=PhysicalConstantEncoder, indent=2)
 
 def main():
+    # Generate master id lookup file
+    ids_to_json()
+    
+    # Process allascii files    
     if not args.year:
-        years = [1998,2002,2006,2010,2014,2018,2022]
+        years = ALL_VERSIONS
     else:
         years = args.year
     for year in years:
         input_filename = f'allascii_{year}.txt'
         input_file = os.path.join(script_dir, str(year), input_filename)
+        logging.info("="*80)
+        logging.info(f"Processing {input_file}")
 
         output_filename =   f'{os.path.splitext(input_filename)[0]}.json'
         output_file = os.path.join(script_dir, str(year), output_filename)
 
         constants = read_allascii_file(input_file, year)
+        
+        # lookup NIST identfier
+        for constant in constants:
+            constant.nist_id = lookup_id(constant.quantity)
+            if not constant.nist_id:
+                logging.warning(f"NIST identifier not found for '{constant.quantity}'")      
 
         # Save to JSON
         allascii_to_json(constants, output_file)
 
-        print(f"Data has been saved to {output_file}")
+        logging.info(f"Data has been saved to {output_file}")
 
 if __name__ == "__main__":
     script_dir = os.path.dirname(__file__)
 
     parser = argparse.ArgumentParser(description='Process physical constants from a text file and save to JSON.')
     parser.add_argument('year', nargs='*', type=int, help='The year(s) to process')
+    parser.add_argument('-ll','--loglevel', help="Python logging level", default="INFO")
     
     args = parser.parse_args()
 
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
+    if args.loglevel:
+        logging.getLogger().setLevel(args.loglevel.upper()) 
+    
     main()
+    
